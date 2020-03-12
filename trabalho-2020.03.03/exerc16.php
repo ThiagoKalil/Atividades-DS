5591adac1a0cdcd3775edc1b581508bdff6c2bd1
ThiagoKalil <ttkalilsamara@gmail.com>
2020-03-12 19:34:44 -0300

    alteração

--- trabalho-2020.03.03/exerc16.php
+++ trabalho-2020.03.03/exerc16.php
@@ -1,5 +1,14 @@
 <?php
+header ("Content-Type: text/html;charset=utf-8");
+
     $numero = 8;  
+
+    if ($numero >= 0){
     for($i=0; $i<=10; $i++)
-    echo $numero." x ".$i." = ".($numero*$i)."<br />";
+    echo $numero." x ".$i." = ".($numero*$i)."<br>";
+    exit;
+    }
+
+    echo "Número inválido";
+    exit;
 ?>
